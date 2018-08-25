dfc19c052d1fd4df446b3b67b25be3679848d844
Bruno Staub <bruno.staub@gmail.com>
2018-08-25 11:34:02 +0200

    BookMonkey Angular4 Buch bis S.87

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {SuiModule} from 'ng2-semantic-ui';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent
   ],
   imports: [
-    BrowserModule
+    SuiModule,BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
